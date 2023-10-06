fd13daafd2780f73106c20440883af55d4d634c1
DevMendozaNP <cesmend.799@gmail.com>
2023-10-06 17:47:58 -0500

    Inicio del State
    REPASA ESTA CLASE

---
+++ Assets/Scripts/Enemy/IdleState.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class IdleState : State
+{
+    public override void OnStart()
+    {
+        throw new System.NotImplementedException();
+    }
+
+    public override void OnUpdate()
+    {
+        throw new System.NotImplementedException();
+    }
+
+    public override void OnFinish()
+    {
+        throw new System.NotImplementedException();
+    }
+}
